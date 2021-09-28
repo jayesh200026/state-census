a3834df54e659a0fa5bdcaf8b3577ed1cdfd542e
jayesh200026 <jayeshkumar8660@gmail.com>
2021-09-28 20:52:50 +0530

    Added feature to read state code csv file and map to stateCode class using existing readcsv method using generics

--- src/main/java/com/census/MyException.java
+++ src/main/java/com/census/MyException.java
@@ -3,7 +3,7 @@ package com.census;
 public class MyException extends Exception{
 	
 	public enum ExceptionType {
-        FILE_NOT_FOUND,INCORRECT_TYPE,DELEMETER_NOT_FOUND,INCORRECT_HEADER,BINDING_BROBLEM_AT_RUNTIME
+        FILE_NOT_FOUND,INCORRECT_TYPE,DELIMITER_NOT_FOUND,INCORRECT_HEADER
     }
 	
 	 public ExceptionType type;
